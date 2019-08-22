682cafb53de032e0c97a0dfeb7e1809edf76f460
Andrew Peterson and Ethan Samuels-Ellingson <ethan@>
2019-08-22 16:45:54 -0700

    add scrambler functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
   $("form#inputForm").submit(function(event){
     event.preventDefault();
     var inputString = $("input#inputText").val();
-
     var output = encryptor(inputString);
     $("p#output").text("encoded message =" + output);
+
 
   });
 
@@ -13,9 +13,25 @@ $(document).ready(function(){
 
 //backend
 function encryptor(inputString) {
-  var outputString;
-  outputString = inputString.replace(/[\?\!\ \.\,]+/g, "");
-  var length = outputString.length
+  var newString = inputString.replace(/[\?\!\ \.\,\']+/g, "");
+  var length = newString.length;
+  console.log(length)
+;  var cols = Math.ceil(Math.sqrt(length));
+  console.log(cols);
+  var rows = Math.ceil(length / cols);
+  console.log(rows);
+  var outputString = "";
+  for (var i = 0; i < cols; i++){
+    for (var j = 0; j < rows * cols; j += cols -1){
+      outputString += newString[j+i];
+    }
+  }
+
+
+
+
+
+
 
 
 
